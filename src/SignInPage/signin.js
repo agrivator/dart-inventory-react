ef56d96d9e3f7223830c03a9165eebef660f87f1
Kiran Johns <kiranjohns369@gmail.com>
2020-06-06 11:50:29 +0530

    signin page

--- src/SignInPage/signin.js
+++ src/SignInPage/signin.js
@@ -1,11 +1,35 @@
 import React from 'react'
+import styles from  './signinStyle.module.css'
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBCardBody, MDBCard } from 'mdbreact';
 
-import siginStyle from "./signinStyle.module.css"
-//className={ProductInventoryPage.className} using module.css to avoid collision of same css variable in multiple files
-
-
-const SignInPage = () =>{
-    return <div><h1>Signin Page</h1></div>
-}
+const SignInPage = () => {
+return (
+<MDBContainer className={styles.form}>
+  <MDBRow>
+    <MDBCol>
+    <MDBCard>
+    <MDBCardBody className={styles.formBody}>
+      <form>
+        <p className="h4 text-center">Sign in</p>
+        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+          Your email
+        </label>
+        <input type="email" id="defaultFormLoginEmailEx" className="form-control" placeholder="Username" />
+        <br />
+        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+          Your password
+        </label>
+        <input type="password" id="defaultFormLoginPasswordEx" className="form-control" placeholder="Password" />
+        <div className="text-center" className={styles.button}>
+          <MDBBtn color="info" type="submit" block>Login</MDBBtn>
+        </div>
+      </form>
+      </MDBCardBody>
+      </MDBCard>
+    </MDBCol>
+  </MDBRow>
+</MDBContainer>
+);
+};
 
 export default SignInPage
